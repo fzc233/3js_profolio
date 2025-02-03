a3f7725a1cab139ffdac79a6814b4db05189171a
Conor Fei <1421446269@qq.com>
2025-02-04 00:36:44 +0800

    Reinitialize repository

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+    return (
+        <div className="min-h-screen bg-[var(--colors-black)] text-[var(--colors-white)] flex justify-center items-center">
+            <h1 className="text-4xl font-[var(--font-family-generalsans)]">
+                Welcome to My Three.js Portfolio
+            </h1>
+        </div>
+    );
+}
