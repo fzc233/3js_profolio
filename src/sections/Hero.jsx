e16f2e11484632b69004ed1a01201a53c2ba6f05
Conor Fei <1421446269@qq.com>
2025-02-06 21:22:54 +0800

    update some detail

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,10 +1,17 @@
 import {Canvas} from "@react-three/fiber";
-import {PerspectiveCamera} from "@react-three/drei";
 import HackerRoom from "../components/HackerRoom.jsx";
 import {Suspense} from "react";
 import CanvasLoader from "../components/CanvasLoader.jsx";
+import {useMediaQuery} from "react-responsive";
+import {calculateSizes} from "../constants/index.js";
+import Target from "../components/target.jsx";
 
 const Hero = () => {
+    const isSmall = useMediaQuery({maxWidth:400})
+    const isTablet = useMediaQuery({minWidth:768,maxWidth:1024})
+    const isMobile = useMediaQuery({ maxWidth:768 })
+    const sizes = calculateSizes(isSmall, isMobile, isTablet)
+
     return (
         <section className="min-h-screen w-full flex flex-col relative">
             <div className="w-full mx-auto flex flex-col sm:sm-36 mt-20 c-space gap-3">
@@ -14,13 +21,23 @@ const Hero = () => {
                 <p className="hero_tag text-gray_gradient">Building Products & Brands</p>
             </div>
             <div className="w-full h-full absolute inset-0">
+                {/*<Leva/>*/}
             {/* use 3js*/}
                 <Canvas className="w-full h-full" camera={{ position: [0, 5, 10], fov: 50 }} >
 
                     <Suspense fallback={<CanvasLoader/>}>
                         <ambientLight intensity={1.5}/>
-                        <directionalLight position={[5, 5, 5]} intensity={2}/>
-                        <HackerRoom scale={0.1} position={[0, -5, 0]}/>
+                        <directionalLight position={[10,10,10]} intensity={1}/>
+                        <HackerRoom //scale={0.1}
+                            scale={sizes.deskScale}
+                                    // rotation={[0,0,0]}
+                                    // position={[0,-4, 2]}
+                            rotation={[0,0,0]}
+                            position={sizes.deskPosition}/>
+                        <group>
+                            <Target position={sizes.targetPosition}/>
+                        </group>
+
                     </Suspense>
                 </Canvas>
             </div>
