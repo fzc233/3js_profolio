3815c12eb4e528847f601b2dfc422dc80787adad
Conor <1421446269@qq.com>
2025-02-15 23:59:33 +1100

    update personal detail

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -6,6 +6,10 @@ const About = () => {
                 <div className="col-span-1 xl:row-span-3">
                     <div className="grid-container">
                         <img src="/assets/grid1.png" alt="grid-1" className="w-full sm:h-[276px] h-fit object-contain"/>
+                        <div>
+                            <p className="grid-headtext">Hello,I am Conor</p>
+                            <p className="grid-subtext">Over 3 years of experience in web development, I have honed my skills in frontend and backend development like ReactJS and .Net</p>
+                        </div>
                     </div>
                 </div>
             </div>
