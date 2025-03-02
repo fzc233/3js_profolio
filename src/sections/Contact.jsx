6604915d7fff219c77b22b59f45501ccb6cadd67
Conor <1421446269@qq.com>
2025-03-02 23:52:00 +1100

    update contact page

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import {useRef, useState} from 'react'
 
 const Contact = () => {
+    const formRef =useRef();
+    const [loading, setLoading] = useState(false)
+    const [form, setForm] = useState(
+        {
+            name: '',
+            email: '',
+            message: ''
+        }
+    )
+    const handleChange = ()=> {
+
+    }
+    const handleSubmit = ()=> {
+
+    }
     return (
-        <div>Contact</div>
+       <section className="c-space my-20">
+           <div className="relative min-h-screen flex items-center justify-center flex-col">
+               <img src="/assets/terminal.png" alt="terminal background" className="absolute inset-0 min-h-screen"/>
+               <div className="contact-container">
+                   <h3 className="head-text">Get in touch</h3>
+                   <p className="text-lg text-white-600 mt-3">Feel free to reach out with any questions or just to say
+                       hello—my inbox is always open!</p>
+                   <form ref={formRef} onSubmit={handleSubmit} className="mt-12 flex flex-col space-y-7">
+                    <label className='space-y-3'>
+                        <span className="field-label">Full Name</span>
+                        <input
+                            type="text"
+                            name="name"
+                            value={form.name}
+                            onChange={handleChange}
+                            required
+                            className="field-input"
+                            placeholder="Your name"
+                        />
+                    </label>
+                   </form>
+               </div>
+           </div>
+       </section>
     )
 }
 export default Contact
