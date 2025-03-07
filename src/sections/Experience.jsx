fe13572259df8e36d04c1585f8229964bf0ac653
Conor <1421446269@qq.com>
2025-03-07 23:59:04 +1100

    update

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import {Canvas} from "@react-three/fiber";
 
 const Experience = () => {
     return (
-        <div>Experience</div>
+        <section className="c-space my-20">
+            <div className="w-full text-white-600">
+                <h3 className="head-text">My work Experience</h3>
+                <div className="work-container">
+                    <div className="work-canvas">
+                        <Canvas></Canvas>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 export default Experience
