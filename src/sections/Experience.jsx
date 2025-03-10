0ae1660df3f58e61363e8670b5c79038d5571222
Conor <1421446269@qq.com>
2025-03-10 23:57:00 +1100

    update

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -15,8 +15,20 @@ const Experience = () => {
                     </div>
                     <div className="work-content">
                         <div className="sm:py-10 py-5 sm:px-5 px-2.5">
-                            {workExperiences.map(()=>(
-                                <div></div>
+                            {workExperiences.map(({id,name,pos,icon,duration,title,animation})=>(
+                                <div key={id} className="work-content_container group">
+                                    <div className="flex flex-col h-full justify-start items-center py-2">
+                                        <div className="work-content_logo">
+                                            <img src={icon} alt="logo" className="w-full h-full"/>
+                                        </div>
+                                        <div className="work-content_title"/>
+                                    </div>
+                                    <div className="sm:p-5 px-2.5 py-5">
+                                        <p className="font-bold text-white-800">{name}</p>
+                                        <p className="text-sm mb-5">{pos} -- {duration}</p>
+                                        <p className="group-hover:text-white transition east-in-out duration-500">{title}</p>
+                                    </div>
+                                </div>
                             ))}
                         </div>
                     </div>
